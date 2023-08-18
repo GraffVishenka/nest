70d08ddf4fdf53f5dd3d2e9535960dbc7b0405a5
Graff Vishenka <maxvisnh@gmail.com>
2023-08-18 17:40:24 +0500

    Finished server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "./pipes/validation.pipe";
 
-async function bootstrap() {
+async function start() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle("Backend on NestJS")
+    .setDescription("Документация REST API")
+    .setVersion("1.0.0")
+    .addTag("Cherry")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/api/docs", app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
 }
-bootstrap();
+
+start();
